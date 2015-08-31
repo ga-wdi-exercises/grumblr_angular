2470cf99bdfb023b2841e0ec8a0538ea2550a69e
RobertAKARobin <robertgfthomas@gmail.com>
2015-08-31 00:45:39 -0400

    Revert "allow any http methods"
    This reverts commit 58a6f0acf574bd51a1ab8c6a60b43a9dd6740fea.

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module GrumblerRailsApi
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => :any
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
 
