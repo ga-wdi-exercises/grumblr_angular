785bcf8e97ea2d7253deef3e503b61cab8698d6c
RobertAKARobin <robertgfthomas@gmail.com>
2015-08-31 14:35:21 -0400

    added humorous seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,48 @@
 require('buzzfeed_title_generator')
+require('httparty')
 
-Grumble.destroy_all
-Comment.destroy_all
+#Grumble.destroy_all
+#Comment.destroy_all
 
 names = %w(Jesse Adam Andy Robin Adrian Matt)
-avatars = ['http://placecage.com/100/100', 'http://fillmurray.com/100/100', 'http://placebear.com/100/100', 'http://www.stevensegallery.com/100/100']
-title = %w(WOW OMG SUCH AMAZE)
+
+def advice
+  JSON.parse(HTTParty.get("http://api.adviceslip.com/advice").body)["slip"]["advice"]
+end
+
+def photo
+  JSON.parse(HTTParty.get("http://www.splashbase.co/api/v1/images/random?images_only=true").body).url
+end
+
+def comment
+  comments = [
+    "That photo reminds me of the time I saw",
+    "Your post would be better if it included",
+    "There is no God. There is only",
+    "I'd encourage you to think more broadly of",
+    "This just goes to show your ignorance about",
+    "Who wrote this? It sounds like it was written by",
+    "This is all well and good, but what about",
+    "People said the same thing when there was",
+    "I'm not sure how to feel about this. My opinions are usually informed by",
+    "I feel like a more appropriate picture for this post would be",
+    "It seems like all you see on the news these days is",
+    "Ugh. Modern society is so obsessed with",
+    "I've responded to this in my post about",
+    "Didn't you know? The government is secretly controlled by"
+  ]
+  prompt = HTTParty.get("http://www.ineedaprompt.com?api").body
+  comments.sample + " " + prompt[0,1].downcase + prompt[1..-1]
+end
 
 names.each do |name|
-  current_grumble = Grumble.create!(authorName: name, title: title.sample(2).join(' '), content: BuzzfeedTitleGenerator.make_title, photoUrl: "http://placebear.com/400/400")
+  current_grumble = Grumble.create!(
+    authorName: name,
+    title: BuzzfeedTitleGenerator.make_title,
+    content: advice,
+    photoUrl: photo
+  )
   (rand(4) + 2).times do
-    current_grumble.comments.create!(authorName: names.sample, content: "hello")
+    current_grumble.comments.create!(authorName: names.sample, content: comment)
   end
 end
