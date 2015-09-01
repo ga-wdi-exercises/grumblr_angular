4ccdef7756a685ef3576e76b6e90116cc9461fe4
mattscilipoti <matt.scilipoti@ga.co>
2015-09-01 11:24:12 -0400

    docs: add Readme, root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :grumbles, except: [:new, :edit, :show] do
     resources :comments, except: [:new, :edit, :show]
   end
+  root to: redirect('/grumbles')
 end
