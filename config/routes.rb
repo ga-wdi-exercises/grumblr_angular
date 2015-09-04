327ac8a73248cf193085cf941e454b4b19dd6c3e
Jesse Shawl <jesse@jshawl.com>
2015-09-03 21:16:19 -0400

    add show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :grumbles, except: [:new, :edit, :show] do
-    resources :comments, except: [:new, :edit, :show]
+  resources :grumbles, except: [:new, :edit] do
+    resources :comments, except: [:new, :edit]
   end
   root to: redirect('/grumbles')
 end
