8ac3894172a5f93ce075ab3345a77b3b0f454295
Thomas Solow <thomassolow@gmail.com>
2015-12-15 16:20:37 -0500

    create is working

---
+++ js/grumbles/show.controller.js
@@ -0,0 +1,13 @@
+"use strict";
+
+(function(){
+  angular
+  .module("grumbles")
+  .controller("GrumbleShowController", [
+    "$stateParams",
+    GrumbleShowControllerFunction
+  ]);
+
+  function GrumbleShowControllerFunction($stateParams){
+    this.grumble = grumbles[$stateParams.id];  }
+}());
