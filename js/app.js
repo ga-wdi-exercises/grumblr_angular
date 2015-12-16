8c3eb38e80544d71c1c03f39fa87e5abec7e0577
Adrian Maseda <amaseda@gmail.com>
2015-12-15 19:52:58 -0500

    add resource class solution

--- js/app.js
+++ js/app.js
@@ -21,7 +21,21 @@
     })
     .state("grumbleShow", {
       url: "/grumbles/:id",
-      templateUrl: "js/grumbles/show.html"
+      templateUrl: "js/grumbles/show.html",
+      controller: "GrumbleShowController",
+      controllerAs: "GrumbleShowViewModel"
+    })
+    .state("grumbleNew", {
+      url: "/new",
+      templateUrl: "js/grumbles/new.html",
+      controller: "GrumbleNewController",
+      controllerAs: "GrumbleNewViewModel"
+    })
+    .state("grumbleEdit", {
+      url: "/grumbles/:id/edit",
+      templateUrl: "js/grumbles/edit.html",
+      controller: "GrumbleEditController",
+      controllerAs: "GrumbleEditViewModel"
     });
   }
 }());
