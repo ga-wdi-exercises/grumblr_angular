28b776837c63e4ebb3d576fbca6b8b07791e8907
Negar Kalbasi <negar.kalbasi@gmail.com>
2015-12-16 08:18:49 -0500

    solution for CRD functionality

---
+++ js/grumbles/show.controller.js
@@ -0,0 +1,16 @@
+"use strict";
+
+(function(){
+    angular
+    .module("grumbles")
+    .controller("GrumbleShowController", [
+      "$stateParams",
+      GrumbleShowControllerFunction
+    ])
+
+    function GrumbleShowControllerFunction($stateParams){
+      this.grumble = grumbles[$stateParams.id]
+    }
+
+
+}())
