8613d9a0f06cea813fbaf0daabdfa0a0a0c72ed2
Alexis Johnson <alexis.johnson052@gmail.com>
2015-12-15 22:37:40 -0500

    create, delete

---
+++ js/grumbles/show.controller.js
@@ -0,0 +1,12 @@
+"use strict";
+
+(function(){
+  angular.module("grumbles")
+  .controller("GrumbleShowController", [
+    "$stateParams",
+    GrumbleShowControllerFunction
+  ]);
+  function GrumbleShowControllerFunction($stateParams){
+    this.grumble = grumbles[$stateParams.id];
+  }
+})();
