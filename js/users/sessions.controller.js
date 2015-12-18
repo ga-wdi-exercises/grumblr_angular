28546a3e205f82884972ff0d8a996f3121fe52ff
Adam Bray <adam.bray@yale.edu>
2015-12-17 22:29:12 -0500

    signin: rediect to grumbles index after signin

--- js/users/sessions.controller.js
+++ js/users/sessions.controller.js
@@ -1,13 +1,13 @@
 (function(){
   angular
     .module("grumblr")
-    .controller("SessionsController", function($auth){
+    .controller("SessionsController", function($auth, $state){
       this.signinForm = {};
 
       this.signin = function() {
         $auth.submitLogin(this.signinForm)
         .then(function(resp) {
-          console.log("Signin success:", resp);
+          $state.go('grumbleIndex');
         })
         .catch(function(resp) {
           console.log("Signin failure:", resp);
