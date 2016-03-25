db725829607839e66bff9e5c2278fdc551eae770
Nick Olds <nolds9@yahoo.com>
2016-03-25 16:30:43 -0400

    adds index and show: states and templates - no data

---
+++ js/app.js
@@ -0,0 +1,24 @@
+"use strict";
+
+(function(){
+  angular
+    .module("grumblr", [
+      'ui.router'
+    ])
+    .config([
+      "$stateProvider",
+      RouterFunction
+    ]);
+
+    function RouterFunction($stateProvider){
+      $stateProvider
+        .state("grumbleIndex", {
+          url: "/grumbles",
+          templateUrl: "js/grumbles/index.html"
+        })
+        .state("grumbleShow", {
+          url: "/grumbles/:id",
+          templateUrl: "js/grumbles/show.html"
+        })
+    }
+})();
