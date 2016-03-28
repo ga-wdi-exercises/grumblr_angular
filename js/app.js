61e2d9a9359d8b00239d22602f88cd9ff7c0616e
Christine Movius <christine.movius@gmail.com>
2016-03-28 16:55:57 -0400

    New/create completed.

--- js/app.js
+++ js/app.js
@@ -19,6 +19,12 @@
       controller: "GrumbleIndexController",
       controllerAs: "GrumbleIndexViewModel"
     })
+    .state("grumbleNew", {
+      url: "/grumbles/new",
+      templateUrl: "js/grumbles/new.html",
+      controller: "GrumbleNewController",
+      controllerAs: "GrumbleNewViewModel"
+    })
     .state("grumbleShow", {
       url: "/grumbles/:id",
       templateUrl: "js/grumbles/show.html",
