4c527769973523fa0964a91acda8fa8d8f07c5fc
Mohamed Chalal <Mohamedrchalal@gmail.com>
2016-03-29 19:34:51 -0400

    Solution Branch

---
+++ js/app.js
@@ -0,0 +1,28 @@
+"use strict";
+
+(function(){
+  angular
+  .module("grumblr", [
+    "ui.router",
+    "grumbles"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("grumbleIndex",{
+      url: "/grumbles",
+      templateUrl: "js/grumbles/index.html",
+      controller: "GrumbleIndexController",
+      controllerAs: "GrumbleIndexViewModel"
+    })
+    .state("grumbleShow", {
+    url: "/grumbles/:id",
+    templateUrl: "js/grumbles/show.html",
+    controller: "GrumbleShowController",
+    controllerAs: "Grumble"
+  });
+  }
+})();
