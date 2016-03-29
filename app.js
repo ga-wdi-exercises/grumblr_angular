ea91d8e80bba3e9e84fd4091e8e45798a8ca33e4
Cam <camlatimer@gmail.com>
2016-03-29 19:11:25 -0400

    made files for index and show routes/states and views and added file for grumbles module

--- app.js
+++ app.js
@@ -5,4 +5,21 @@
   .module("grumblr", [
     "ui.router"
   ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("grumbleIndex", {
+      url:"/grumbles",
+      templateUrl:"js/grumbles/index.html"
+    })
+    .state("grumbleShow", {
+      url: "/grumbles/:id",
+      templateUrl: "jus/grumbles/show.html"
+    });
+  }
+
 }());
