b638d81f6342cad362dee7ab2023bf0141057f2d
Christine Movius <christine.movius@gmail.com>
2016-03-29 15:22:53 -0400

    directive methods.

--- js/grumbles/grumble.directive.js
+++ js/grumbles/grumble.directive.js
@@ -1,9 +1,18 @@
+"use strict";
+
 (function(){
   angular
   .module('grumbles')
   .directive('myCustomDirective', function(){
     return {
-      template: '<h1>Hi There!</h1>'
+      template: "<h1 ng-click='complementMe()'>My name is {{myName}}</h1>",
+      link: function(scope){
+        console.log('directive used');
+        scope.myName = 'Slim Shady';
+        scope.complementMe = function(){
+          alert('You’re looking good today');
+        }
+      }
     }
   });
 })();
