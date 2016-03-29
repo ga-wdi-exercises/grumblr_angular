9dbb9318db118b2016066cf615de1dd4267b7875
Cam <camlatimer@gmail.com>
2016-03-29 19:50:42 -0400

    added script tags for show controller and grumble factory into main index.html and made file for show controller

--- js/grumbles/index.controller.js
+++ js/grumbles/index.controller.js
@@ -8,6 +8,7 @@
     GrumbleIndexControllerFunction
   ]);
   function GrumbleIndexControllerFunction(GrumbleFactory){
+    // makes index controller get grumbles from grumble factory's api request/response
     this.grumbles = GrumbleFactory.query();
     console.log("im in the controller!");
   }
