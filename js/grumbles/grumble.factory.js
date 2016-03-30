1adb29f7d7723d6609f814faf78d9270181a667a
Bipin Neupane <icomp2@hotmail.com>
2016-03-30 19:39:56 -0400

    submitting hw

---
+++ js/grumbles/grumble.factory.js
@@ -0,0 +1,16 @@
+"use strict";
+
+(function(){
+  angular
+    .module( "grumbles" )
+    .factory( "GrumbleFactory", [
+      "$resource",
+      FactoryFunction
+    ])
+
+  function FactoryFunction( $resource ){
+    return $resource( "http://localhost:3000/grumbles/:id", {}, {
+        update: { method: "PUT" }
+    });
+  }
+}());
