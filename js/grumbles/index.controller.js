58fdc930108dbdf73c64a05f6f1a0a6da34c5bdb
Kimberly Cabbagestalk <kimcabb@gmail.com>
2016-03-30 14:01:56 -0400

    initial commit

---
+++ js/grumbles/index.controller.js
@@ -0,0 +1,15 @@
+"use strict";
+
+(function(){
+  angular
+    .module( "grumbles" )
+    .controller( "GrumbleIndexController", [
+      "GrumbleFactory",
+      GrumbleIndexControllerFunction
+    ]);
+    // Whenever `.grumbles` is called on our ViewModel, it returns the response from `.query()`
+    function GrumbleIndexControllerFunction( GrumbleFactory ){
+      this.grumbles = GrumbleFactory.query();
+    }
+
+}());
