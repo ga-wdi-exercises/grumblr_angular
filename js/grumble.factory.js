d7c153465b0592536d3b8305bb6f45e8ea9cacf4
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-03-30 17:50:15 -0400

    transfer from inclass grumblr work

---
+++ js/grumble.factory.js
@@ -0,0 +1,19 @@
+"use strict";
+
+(function(){
+  angular
+    .module( "grumbles" )
+    .factory( "GrumbleFactory", [
+      "$resource",
+      FactoryFunction
+    ])
+
+  // function FactoryFunction( $resource ){
+  //   return $resource( "http://localhost:3000/grumbles/:id", {}, {
+  //     update: { method: "PUT" }
+  //   });
+  // }
+  function FactoryFunction( $resource ){
+    return $resource( "http://localhost:3000/grumbles/:id" );
+  }
+}());
