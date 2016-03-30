6988b23b9ecf6de781b9529c3827be1707e65502
Charles <charlesthwang@gmail.com>
2016-03-30 19:46:01 -0400

    added module, config, and created index route/view

---
+++ js/app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+(function(){
+  angular
+  .module("grumblr", [
+    "ui.router"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("grumbleIndex", {
+      url: "/grumbles",
+      templateUrl: "js/grumbles/index.html"
+    });
+  }
+})();
