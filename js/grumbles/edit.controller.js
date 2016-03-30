1a2e7e2e84a2820c6d936936cd9b03e5fba2001a
Patrick Moran <patrick_moran@mac.com>
2016-03-30 14:15:49 -0400

    delete post working

--- js/grumbles/edit.controller.js
+++ js/grumbles/edit.controller.js
@@ -5,17 +5,20 @@
     .module( "grumbles" )
     .controller( "GrumbleEditController", [
       "GrumbleFactory",
+      "$state",
       "$stateParams",
       GrumbleEditControllerFunction
     ]);
 
-  function GrumbleEditControllerFunction( GrumbleFactory, $stateParams ){
+  function GrumbleEditControllerFunction( GrumbleFactory, $state, $stateParams ){
     this.grumble = GrumbleFactory.get({id: $stateParams.id});
     this.update = function(){
       this.grumble.$update({id: $stateParams.id})
     }
     this.destroy = function(){
-      this.grumble.$delete({id: $stateParams.id});
+      this.grumble.$delete({id: $stateParams.id}).then(function() {
+        $state.go("grumbleIndex", {}, {reload: true});
+      });
     }
   }
 }());
