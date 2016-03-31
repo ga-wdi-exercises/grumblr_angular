0b96691cd54be8b5328461609651a7572e2657ef
Charles <charlesthwang@gmail.com>
2016-03-31 01:20:49 -0400

    create edit state route, edit controller, and edit viewi page

---
+++ js/grumbles/edit.controller.js
@@ -0,0 +1,18 @@
+"use strict";
+
+(function(){
+  angular
+  .module("grumbles")
+  .controller("GrumbleEditController", [
+    "GrumbleFactory",
+    "$stateParams",
+    GrumbleEditControllerFunction
+  ]);
+
+  function GrumbleEditControllerFunction(GrumbleFactory, $stateParams){
+    this.grumble = GrumbleFactory.get({id: $stateParams.id});
+    this.update = function(){
+      this.grumble.$update({id: $stateParams.id})
+    }
+  }
+})();
