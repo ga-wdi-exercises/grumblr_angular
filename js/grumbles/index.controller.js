9c31771a67b31e3d050a3a06a7c51c21a9e1d8fb
Anissa Jones <anissajones10@gmail.com>
2016-03-30 20:04:28 -0400

    Adds . Unsure what it did.

--- js/grumbles/index.controller.js
+++ js/grumbles/index.controller.js
@@ -4,10 +4,14 @@
   angular
   .module("grumbles")
   .controller("GrumbleIndexController", [
+    '$state',
     GrumbleIndexControllerFunction
   ]);
 
-  function GrumbleIndexControllerFunction(){
+  function GrumbleIndexControllerFunction($state){
     this.grumbles = grumblesdata;
+    this.goToIndex = function(){
+      $state.go("grumbleIndex");
+    };
   }
 }());
