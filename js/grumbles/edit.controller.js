5de57c8a7e55a3ca57b4fb66e57fbfc6da3edd83
Andrew Smith-Mui <arsm800@gmail.com>
2016-03-30 22:57:26 -0400

    add edit links for each grumble.

--- js/grumbles/edit.controller.js
+++ js/grumbles/edit.controller.js
@@ -12,10 +12,10 @@
   function GrumbleEditControllerFunction( GrumbleFactory, $stateParams ){
     this.grumble = GrumbleFactory.get({id: $stateParams.id});
     this.update = function(){
-      this.grumble.$update({id: $stateParams.id})
-    }
+      this.grumble.$update({id: $stateParams.id});
+    };
     this.destroy = function(){
       this.grumble.$delete({id: $stateParams.id});
-    }
+    };
   }
 }());
