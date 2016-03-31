66aaafb1220f5810119ac5628c2e2d39427132e2
Ryan Krolick <rkrolick@gmail.com>
2016-03-31 01:22:10 -0400

    can create new grumble

--- js/grumbles/edit.controller.js
+++ js/grumbles/edit.controller.js
@@ -7,25 +7,22 @@
       "GrumbleFactory",
       "$stateParams",
       //"$stateProvider", TODO: Why does this cause errors?
+      "$state",
       GrumbleEditControllerFunction
     ]);
 
-  function GrumbleEditControllerFunction( GrumbleFactory, $stateParams){
+  function GrumbleEditControllerFunction( GrumbleFactory, $stateParams, $state){
     this.grumble = GrumbleFactory.get({id: $stateParams.id});
     this.update = function(){
       this.grumble.$update({id: $stateParams.id});
-      //this.goHome();
+      this.goHome();
     }
     this.destroy = function(){
       this.grumble.$delete({id: $stateParams.id});
-      //this.goHome();
+      this.goHome();
     }
-    // this.goHome = function(){
-    //   $state.transitionTo("GrumbleIndex", {}, {
-    //     reload: true,
-    //     inherit: false,
-    //     notify: true
-    //   });
-    // }
+    this.goHome = function(){
+      $state.go("grumbleIndex", {}, {reload: true}); // TODO: This will redirect me to the index, but I must manually press reload to see changes. Why?
+    }
   }
 }());
--- js/grumbles/new.controller.js
+++ js/grumbles/new.controller.js
@@ -6,13 +6,15 @@
     .controller( "GrumbleNewController", [
       "GrumbleFactory",
       "$stateParams",
+      "$state",
       GrumbleNewControllerFunction
     ])
 
-    function GrumbleNewControllerFunction( GrumbleFactory, $stateParams ){
+    function GrumbleNewControllerFunction( GrumbleFactory, $stateParams, $state ){
       this.grumble = new GrumbleFactory();
       this.create = function(){
         this.grumble.$save()
+        $state.go("grumbleIndex", {}, {reload: true});
       }
     }
 }());
