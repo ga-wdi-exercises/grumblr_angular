87a539c58768fb992257883d619797e640dbe5c1
Craig Eisen <Ceisen91@gmail.com>
2016-03-31 00:10:40 -0400

    Migrated in Grumblr folder as done on another location on my desktop into this repo. Also added in stylesheet

---
+++ grumblr_angular-2.0.0/js/grumbles/index.controller.js
@@ -0,0 +1,14 @@
+"use strict";
+
+(function(){
+  angular
+  .module("grumbles")
+  .controller("GrumbleIndexController", [
+    "GrumbleFactory",
+    GrumbleIndexControllerFunction
+  ]);
+
+  function GrumbleIndexControllerFunction(GrumbleFactory){
+    this.grumbles = GrumbleFactory.query();
+  }
+}());
---
+++ grumblr_angular-3.0.0/js/grumbles/form.directive.js
@@ -0,0 +1,37 @@
+"use strict";
+
+(function(){
+  angular
+  .module("grumbles")
+  .directive("grumbleForm", [
+    "GrumbleFactory",
+    "$state",
+    GrumbleFormDirectiveFunction
+  ]);
+
+  function GrumbleFormDirectiveFunction(GrumbleFactory, $state){
+    return{
+      templateUrl: "js/grumbles/form.html",
+      scope: {
+        grumble: "="
+      },
+      link: function(scope){
+        scope.create = function(){
+          scope.grumble.$save(function(response){
+            $state.go("grumbleIndex", {}, {reload: true});
+          });
+        }
+        scope.update = function(){
+          scope.grumble.$update({id: scope.grumble.id}, function(response){
+            console.log(response);
+          });
+        }
+        scope.delete = function(){
+          scope.grumble.$delete({id: scope.grumble.id}, function(){
+            $state.go("grumbleIndex", {}, {reload: true});
+          });
+        }
+      }
+    }
+  }
+}());
