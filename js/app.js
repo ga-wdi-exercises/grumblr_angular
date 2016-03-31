21c81636fcf9db0078aabf4799894bf7e1d661b3
Brendan Magee <bmagee@gmail.com>
2016-03-30 20:26:40 -0400

    added editing grumbles

--- js/app.js
+++ js/app.js
@@ -30,6 +30,12 @@
       templateUrl: "js/grumbles/show.html",
       controller: "GrumbleShowController",
       controllerAs: "GrumbleShowViewModel"
+    })
+    .state("grumbleEdit", {
+      url: "/grumbles/:id",
+      templateUrl: "js/grumbles/edit.html",
+      controller: "GrumbleEditController",
+      controllerAs: "GrumbleEditViewModel"
     });
   }
 }());
