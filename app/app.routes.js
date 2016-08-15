d471b8bdb05f98bcfafdc93684edac6900ca196e
Christopher Phillips <cphillips918@gmail.com>
2016-08-15 04:35:26 -0400

    location provider bugs not sorted

--- app/app.routes.js
+++ app/app.routes.js
@@ -1,13 +1,11 @@
 var app = angular.module('myAppRouter',['ui.router'])
 .config([
     "$stateProvider",
-    "$locationProvider",
     Router])
 function Router($stateProvider, $locationProvider){
-    $locationProvider.html5Mode(true);
     $stateProvider
     .state("grumbleIndex", {
-        url:"/grumbles",
+        url:"/",
         controller: "GrumbleIndexController",
         controllerAs:"GrumbleIndexViewModel",
         templateUrl: "app/components/grumbles/grumbleView.html"
