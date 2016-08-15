78d41159ba8adf2e29e3b20e9606cdb5e3ae51c4
Jacqui Ahn <jsahn23@gmail.com>
2016-08-14 22:03:03 -0400

    ui router, creates and deletes grumbles

---
+++ js/app.js
@@ -0,0 +1,20 @@
+angular.module("grumblr", ["ui.router"])
+.config(["$stateProvider", RouterCallback])
+
+function RouterCallback($stateProvider) {
+  console.log("working")
+  $stateProvider
+  .state("grumbleIndex", {
+    url: '/grumbles',
+    controller: "GrumbleIndexController",
+    controllerAs: "GrumbleIndexViewModel",
+    templateUrl: 'js/grumbles/index.html'
+  })
+
+  .state("grumbleShow",{
+    url: "/grumbles/:id",
+    templateUrl: "js/grumbles/show.html",
+    controller: "GrumbleShowController",
+    controllerAs: "GrumbleShowViewModel",
+  });
+}
