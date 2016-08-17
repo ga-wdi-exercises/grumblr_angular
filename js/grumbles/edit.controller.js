7ccedcf5970c91aa72667d5b8c7b9aaac73fbc41
Marc Fournier <marc.fournier91@gmail.com>
2016-08-17 19:39:15 -0400

    Added edit and delete features

---
+++ js/grumbles/edit.controller.js
@@ -0,0 +1,22 @@
+"use strict";
+console.log("file defined");
+(function(){
+  angular
+  .module("grumbles")
+  .controller("GrumbleEditController", [
+    "GrumbleFactory",
+    "$stateParams",
+    GrumbleEditControllerFunction
+  ]);
+
+  function GrumbleEditControllerFunction( GrumbleFactory, $stateParams ){
+    console.log("Callback");
+    this.grumble = GrumbleFactory.get({id: $stateParams.id});
+    this.update = function(){
+      this.grumble.$update({id: $stateParams.id})
+    }
+    this.destroy = function(){
+      this.grumble.$delete({id: $stateParams.id});
+    }
+  }
+})();
--- js/grumbles/grumble.factory.js
+++ js/grumbles/grumble.factory.js
@@ -7,6 +7,8 @@
     ]);
 
     function GrumbleFactoryFunction( $resource ){
-      return $resource( "http://localhost:3000/grumbles/:id" );
+      return $resource( "http://localhost:3000/grumbles/:id", {}, {
+        update: {method: "PUT"}
+      });
     }
 }());
