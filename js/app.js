62a78821334da282ea4fc9e8d5f3c094c29a6c3d
Jacqui Ahn <jsahn23@gmail.com>
2016-08-17 19:42:31 -0400

    uses factory to do homework

--- js/app.js
+++ js/app.js
@@ -19,9 +19,23 @@
       controller: "GrumbleIndexController",
       controllerAs: "GrumbleIndexViewModel"
     })
+    .state("grumbleNew",{
+      url: "/grumbles/new",
+      templateUrl: "js/grumbles/new.html",
+      controller: "GrumbleNewController",
+      controllerAs: "GrumbleNewViewModel"
+    })
     .state("grumbleShow", {
       url: "/grumbles/:id",
-      templateUrl: "js/grumbles/show.html"
+      templateUrl: "js/grumbles/show.html",
+      controller: "GrumbleShowController",
+      controllerAs: "GrumbleShowViewModel"
+    })
+    .state("grumbleEdit",{
+      url: "/grumbles/:id/edit",
+      templateUrl: "js/grumbles/edit.html",
+      controller: "GrumbleEditController",
+      controllerAs: "GrumbleEditViewModel"
     });
   }
 }());
--- js/grumbles/grumbles.js
+++ js/grumbles/grumbles.js
@@ -1,6 +1,8 @@
 "use strict";
-
+// This is setting the module so in Factory it can be called
 (function(){
   angular
-  .module("grumbles", []);
+  .module("grumbles", [
+    "ngResource"
+  ]);
 }());
