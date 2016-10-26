957e5c697e706feff71c0721789ae676c2734bdb
Nick Olds <nolds9@yahoo.com>
2016-10-26 16:50:25 -0400

    adds firebase solution

--- js/app.js
+++ js/app.js
@@ -1,29 +1,62 @@
-"use strict";
 
-(function(){
   angular
   .module("grumblr", [
     "ui.router",
-    "grumbles"
+    "firebase"
   ])
   .config([
     "$stateProvider",
     RouterFunction
-  ]);
+  ])
+  .controller("GrumbleIndexController", [
+    "$firebaseArray",
+    GrumbleIndexControllerFunction
+  ])
+  .controller("GrumbleShowController", [
+    "$stateParams",
+    "$firebaseObject",  // We are now using $firebaseObject, not $firebaseArray.
+    GrumbleShowControllerFunction
+  ])
 
   function RouterFunction($stateProvider){
     $stateProvider
     .state("grumbleIndex", {
       url: "/grumbles",
-      templateUrl: "js/grumbles/index.html",
+      templateUrl: "js/ng-views/index.html",
       controller: "GrumbleIndexController",
       controllerAs: "GrumbleIndexViewModel"
     })
     .state("grumbleShow", {
       url: "/grumbles/:id",
-      templateUrl: "js/grumbles/show.html",
+      templateUrl: "js/ng-views/show.html",
       controller: "GrumbleShowController",
       controllerAs: "GrumbleShowViewModel"
     });
   }
-}());
+
+  function GrumbleIndexControllerFunction($firebaseArray){
+    let ref = firebase.database().ref().child("grumbles");
+    this.grumbles = $firebaseArray(ref);
+    this.create = function(){
+      this.grumbles.$add(this.newGrumble).then( () => this.newGrumble = {} )
+    }
+
+    // This method is triggered whenever the user clicks "Delete Grumble".
+    // It takes a grumble as an argument.
+    this.delete = function(grumble){
+      this.grumbles.$remove(grumble)
+    }
+  }
+
+  function GrumbleShowControllerFunction($stateParams, $firebaseObject){
+    // This time, ref contains a reference to a specific grumble.
+    let ref = firebase.database().ref().child("grumbles/" + $stateParams.id);
+
+    // Then we retrieve a $firebaseObject based on ref. Once that asynchronous action is done, we save the resulting grumble to `vm.grumble`.
+    $firebaseObject(ref).$loaded().then( grumble => this.grumble = grumble )
+
+    // This method is triggered when the user clicks "Update Grumble".
+    this.update = function(){
+      this.grumble.$save();
+    }
+  }
