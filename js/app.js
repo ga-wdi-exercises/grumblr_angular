7be8ec27144c0df48a2371191594c6b761123886
Sam Catherman <Sam.Catherman@gmail.com>
2016-11-18 17:00:17 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+angular.module("grumblr", ["ui.router"])
+.config([
+  "$stateProvider",
+  Router
+])
+.controller("GrumbleIndexController", [GrumbleIndexControllerFunction])
+.controller("GrumbleShowController", ["$stateParams", GrumbleShowControllerFunction])
+
+
+function Router($stateProvider){
+  console.log("working")
+  $stateProvider
+  .state("grumbleIndex", {
+    url: "/grumbles",
+    controller: "GrumbleIndexController",
+    controllerAs: "vm",
+    templateUrl: "js/ng-views/index.html"
+  })
+  .state("grumbleShow", {
+    url: "/grumbles/:id",
+    controller: "GrumbleShowController",
+    controllerAs: "vm",
+    templateUrl: "js/ng-views/show.html"
+    })
+}
+
+function GrumbleIndexControllerFunction(){
+  console.log("I'm in the controller!");
+  this.grumbles = grumbles;
+}
+
+function GrumbleShowControllerFunction($stateParams){
+  console.log($stateParams);
+  this.grumble = grumbles[$stateParams.id]
+}
