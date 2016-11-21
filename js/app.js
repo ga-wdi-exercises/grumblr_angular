bb05e1792711665e616a081cf278ae14ac399ccf
Jonathan Reiser <jonathanreiser@Jonathans-MacBook-Pro-2.local>
2016-11-20 20:15:11 -0500

    Adds deleteGrumbler functionality, utilizes .go, uses  to hide #

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 angular
   .module("grumblr", ["ui.router"])
-  .config(["$stateProvider", RouterFunction])
+  .config([
+    "$stateProvider",
+    "$locationProvider",
+    RouterFunction
+  ])
   .controller("GrumbleIndexController", [
+    "$state",
     GrumbleIndexControllerFunction
   ])
-  .controller("GrumbleShowController", [
+  .controller("GrumbleShowController",[
     "$stateParams",
     GrumbleShowControllerFunction
   ])
@@ -14,20 +19,29 @@ angular
 
 function GrumbleCreateControllerFunction(){
   this.addGrumble = function() {
-    let grumble = {title: this.newGrumble};
-    grumbles.push(grumble);
+    grumbles.push(this.newGrumble);
   }
 }
 
 function GrumbleShowControllerFunction($stateParams){
-  this.grumble = grumbles[$stateParams.id]
+  console.log($stateParams.grumble);
+  this.grumble = $stateParams.grumble;
+  self = this;
+  this.deleteGrumbler = function() {
+    console.log(grumbles)
+    grumbles.splice(grumbles.indexOf(self.grumble), 1)
+  }
 }
 
-function GrumbleIndexControllerFunction(){
+function GrumbleIndexControllerFunction($state){
   this.grumbles = grumbles;
+  this.change = function(grumble){
+    $state.go("grumbleShow",{grumble: grumble.title})
+  }
 }
 
-function RouterFunction($stateProvider){
+function RouterFunction($stateProvider, $locationProvider){
+  $locationProvider.html5Mode(true);
   $stateProvider
   .state("grumbleIndex", {
     url: "/grumbles",
@@ -35,16 +49,16 @@ function RouterFunction($stateProvider){
     controllerAs: "vm",
     templateUrl: "js/ng-views/index.html"
   })
+  .state("grumbleShow",{
+    url: "/grumbles/:grumble",
+    controller: "GrumbleShowController",
+    controllerAs: "vm",
+    templateUrl: "js/ng-views/show.html"
+  })
   .state("grumbleCreate", {
     url: "/creategrumble",
     templateUrl: "js/ng-views/create.html",
     controller: "GrumbleCreateController",
     controllerAs: "vm"
   })
-  .state("grumbleShow", {
-    url: "/grumbles/:id",
-    templateUrl: "js/ng-views/show.html",
-    controller: "GrumbleShowController",
-    controllerAs: "vm"
-  });
 }
