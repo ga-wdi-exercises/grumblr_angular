b6a72730f5ba8671c971bb587c15ee0e90c167d4
Andy <andrew.sunglae.kim@gmail.com>
2016-11-20 20:57:39 -0500

    moves all starter code to 1 file except views

--- js/app.js
+++ js/app.js
@@ -1,27 +1,34 @@
 "use strict";
 
-(function(){
-  angular
-  .module("grumblr", [
-    "ui.router",
-    "grumbles"
-  ])
+
+angular.module("grumblr", ["ui.router"])
   .config([
     "$stateProvider",
     RouterFunction
-  ]);
+  ])
+  .controller("GrumbleIndexController", [
+    GrumbleIndexControllerFunction
+  ])
 
-  function RouterFunction($stateProvider){
-    $stateProvider
-    .state("grumbleIndex", {
-      url: "/grumbles",
-      templateUrl: "js/grumbles/index.html",
-      controller: "GrumbleIndexController",
-      controllerAs: "GrumbleIndexViewModel"
-    })
-    .state("grumbleShow", {
-      url: "/grumbles/:id",
-      templateUrl: "js/grumbles/show.html"
-    });
-  }
-}());
+function RouterFunction($stateProvider){
+  $stateProvider
+  .state("grumbleIndex", {
+    url: "/grumbles",
+    templateUrl: "js/ng-views/index.html",
+    controller: "GrumbleIndexController",
+    controllerAs: "GrumbleIndexViewModel"
+  })
+  .state("grumbleShow", {
+    url: "/grumbles/:id",
+    templateUrl: "js/ng-views/show.html"
+  });
+}
+
+function GrumbleIndexControllerFunction(){
+  this.grumbles = [
+    {title: "Thesse"},
+    {title: "Are"},
+    {title: "Hardcoded"},
+    {title: "Grumbles"}
+  ]
+}
